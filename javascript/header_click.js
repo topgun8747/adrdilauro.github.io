5d0b9cba7d3b33668a8a15a5348112dada8a7e0b
Adriano di Lauro <adriano@irisconnect.co.uk>
2015-08-30 15:42:23 +0200

    Animation

--- javascript/header_click.js
+++ javascript/header_click.js
@@ -1,5 +1,7 @@
 function reactToHeaderClick (identifier) {
-  var layer = document.getElementById("layer");
+  var layer = document.getElementById("layer"),
+      currentlyOn = document.querySelector(".page-content.on"),
+      newOn = document.querySelector(".page-content." + identifier);
   document.getElementById("signature").style.display = "none";
   layer.style.display = "block";
   makeAnimation(function () {
@@ -7,7 +9,68 @@ function reactToHeaderClick (identifier) {
   });
 
   function makeAnimation (callback) {
-    console.log("animazione " + identifier);
-    callback();
+    var instructions;
+    tradeWind.run([
+      {
+        elements: "#header-up",
+        animations: [
+          {
+            property: "background-color",
+            animationDetails: {
+              duration: "0.8s"
+            },
+            final: "#df0101"
+          }
+        ]
+      }
+    ], function () {
+      document.getElementById("header-up").style.backgroundColor = "black";
+    });
+    instructions = [
+      {
+        elements: ".page-content." + identifier,
+        preStyling: [
+          {
+            property: "z-index",
+            value: "26"
+          },
+          {
+            property: "height",
+            value: "0px"
+          },
+          {
+            property: "border-bottom",
+            value: "5px solid #df0101"
+          },
+          {
+            property: "display",
+            value: "block"
+          }
+        ],
+        animations: [
+          {
+            property: "height",
+            animationDetails: {
+              duration: "2s",
+              delay: "0.6s",
+              easing: "linear"
+            },
+            final: "100%"
+          }
+        ]
+      }
+    ];
+    if (currentlyOn) {
+      instructions.push({
+        //elements:
+      });
+    }
+    tradeWind.run(instructions, function () {
+      if (currentlyOn) currentlyOn.classList.remove("on");
+      newOn.classList.add("on");
+      newOn.style.zIndex = "21";
+      newOn.style.borderBottom = "none";
+      callback();
+    });
   }
 }
